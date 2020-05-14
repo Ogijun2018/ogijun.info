7f4e3c0d1c6eb056a274e8234a9fdd9afc9e12dd
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-05-14 22:47:00 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.508e54413aa49ab1330d0de9ce23a49b.js"
+  "/precache-manifest.7142cb56f3ef2943a5bab61650228a9b.js"
 );
 
 self.addEventListener('message', (event) => {
