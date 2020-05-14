3ceb4f8d263f02f7504dfd1d85ea471241459cbf
Ogijun2018 <ogijun.mk@hotmail.co.jp>
2020-05-14 22:42:41 +0900

    スマホトップページリファクタリング完了

--- src/js/components/PhonePage.js
+++ src/js/components/PhonePage.js
@@ -6,22 +6,26 @@ export default class Page extends React.Component {
     render() {
       return(
         <div className={styles.Page}>
-            <div className={styles.innerPage}>
-                <div className={styles.Headline}>
-                    {this.props.pageName === "About" ? "About Me" : this.props.pageName}
-                </div>
-                <div className={styles.Image}>
+            <div className={styles.ImageSide}>
+                <div className={styles.ImageContainer}>
                     <img src={this.props.imgName} alt=""/>
                 </div>
-                <div className={styles.Line}></div>
-                <div className={styles.Paragraph}>
-                    {this.props.text}
-                </div>
-                {this.props.showMoreBtn &&
-                    <div className={styles.BtnShowMoreContainer}>
-                        <Link to={"/" + this.props.pageName} className={styles.btnShowMore}>show more</Link>
+            </div>
+            <div className={styles.SentenceSide}>
+                <div className={styles.SentenceContainer}>
+                    <div className={styles.Headline}>
+                        {this.props.pageName === "About" ? "About Me" : this.props.pageName}
                     </div>
-                }
+                    <div className={styles.Line}></div>
+                    <div className={styles.Paragraph}>
+                        {this.props.text}
+                    </div>
+                    {this.props.showMoreBtn &&
+                        <div className={styles.BtnShowMoreContainer}>
+                            <Link to={"/" + this.props.pageName} className={styles.btnShowMore}>show more</Link>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
       );
