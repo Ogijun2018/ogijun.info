f0cfa790c508b0080e99ef218e3a561495ac204b
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2021-05-03 01:01:45 +0900

    lintの反映

--- src/js/components/PModal.js
+++ src/js/components/PModal.js
@@ -4,9 +4,9 @@ import { IoIosRemove } from "react-icons/io";
 import { GrClose } from "react-icons/gr";
 
 export default class Page extends React.Component {
-    render() {
-      return(
-        <div>
+  render() {
+    return (
+      <div>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"></link>
         <div className={styles.pCloseButton} onClick={this.props.onPressClose}>
           <GrClose></GrClose>
@@ -18,40 +18,40 @@ export default class Page extends React.Component {
               <hr className={styles.pBar} color="black"></hr>
               <div className={styles.pic}>
                 <div className={styles.pCp_cssslider}>
-                  <input type="radio" name="cp_switch" id="photo1" defaultChecked={true}/>
-                  <label htmlFor="photo1"><img src={this.props.pic[0]} alt=""/></label>
-                  <img src={this.props.pic[0]} alt=""/>
-                  <input type="radio" name="cp_switch" id="photo2"/>
-                  <label htmlFor="photo2"><img src={this.props.pic[1]} alt=""/></label>
-                  <img src={this.props.pic[1]} alt=""/>
+                  <input type="radio" name="cp_switch" id="photo1" defaultChecked={true} />
+                  <label htmlFor="photo1"><img src={this.props.pic[0]} alt="" /></label>
+                  <img src={this.props.pic[0]} alt="" />
+                  <input type="radio" name="cp_switch" id="photo2" />
+                  <label htmlFor="photo2"><img src={this.props.pic[1]} alt="" /></label>
+                  <img src={this.props.pic[1]} alt="" />
                 </div>
               </div>
               <div className={styles.pSubtitle}>
-                <IoIosRemove style={{color: "#FFAB00"}}></IoIosRemove>Description
+                <IoIosRemove style={{ color: "#FFAB00" }}></IoIosRemove>Description
               </div>
               <div className={styles.pSentence}>
                 {this.props.desc}
               </div>
               <div className={styles.pSubtitle}>
-                <IoIosRemove style={{color: "#FFAB00"}}></IoIosRemove>Skill
+                <IoIosRemove style={{ color: "#FFAB00" }}></IoIosRemove>Skill
               </div>
               <div className={styles.pSentence}>
                 {this.props.skill}
               </div>
               <div className={styles.pSubtitle}>
-                <IoIosRemove style={{color: "#FFAB00"}}></IoIosRemove>Demo
+                <IoIosRemove style={{ color: "#FFAB00" }}></IoIosRemove>Demo
               </div>
               <div className={styles.pSentence}>
-                {this.props.url ? 
-                <a style={{color: "#262626"}} href={this.props.url}>{this.props.url}</a>
-                :
-                ""
+                {this.props.url ?
+                  <a style={{ color: "#262626" }} href={this.props.url}>{this.props.url}</a>
+                  :
+                  ""
                 }
               </div>
             </div>
           </div>
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
